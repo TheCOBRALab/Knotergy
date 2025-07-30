003b3d8c10ee32472af2dac4b2d3f958ceafdd34
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-07-30 11:35:19 -0600

    refactor: non_pseudoknot tests condensed

--- test/tests/test_non_pseudo_energies.cpp
+++ test/tests/test_non_pseudo_energies.cpp
@@ -24,39 +24,27 @@ float pipeline(std::string sequence, std::string structure){
 TEST(NonPseudoKnottedEnergies, BasicStack) {
     std::string sequence = "AGGGGGAAAAAAAGGGGGGGGGGAAAAAAAACCCCCAAAAAACCCCCCCCCC";
     std::string structure = ".(((((.........................)))))................";
-    knotergy::load_energy_parameters();
-    knotergy::RNAEntry rna(sequence, structure);
-    knotergy::RNAProcessedEntry processed_rna(rna);
-    knotergy::LoopFactory factory(processed_rna);
-    knotergy::ComputeEnergy energy(factory.get_root_node(), sequence);
-    
-    EXPECT_NEAR(energy.getEnergy(), -8.30, 0.001);
+    float energy = pipeline(sequence, structure);
+
+    EXPECT_NEAR(energy, -8.30, 0.001);
 }
 
 // echo -e "GGGGAGAAAAAAAAAUUUUUU\n((((((.........))))))" | RNAeval
 TEST(NonPseudoKnottedEnergies, StacksFullStructure) {
     std::string sequence = "GGGGAGAAAAAAAAAUUUUUU";
     std::string structure = "((((((.........))))))";
-    knotergy::load_energy_parameters();
-    knotergy::RNAEntry rna(sequence, structure);
-    knotergy::RNAProcessedEntry processed_rna(rna);
-    knotergy::LoopFactory factory(processed_rna);
-    knotergy::ComputeEnergy energy(factory.get_root_node(), sequence);
+    float energy = pipeline(sequence, structure);
 
-    EXPECT_NEAR(energy.getEnergy(), 3.70, 0.001);
+    EXPECT_NEAR(energy, 3.70, 0.001);
 }
 
 // echo -e "GGGGGGAAAAAGGGGGGAAAAGGGGGGCCCCCCAAAAAACCCCCCAAAAGGGGGGAAAAAACCCCCCAAAGGGAAACCCCCCCCC\n((((((.....................))))))................((((((......))))))...(((...)))......" | RNAeval
 TEST(NonPseudoKnottedEnergies, MultipleStacks) {
     std::string sequence = "GGGGGGAAAAAGGGGGGAAAAGGGGGGCCCCCCAAAAAACCCCCCAAAAGGGGGGAAAAAACCCCCCAAAGGGAAACCCCCCCCC";
     std::string structure = "((((((.....................))))))................((((((......))))))...(((...)))......";
-    knotergy::load_energy_parameters();
-    knotergy::RNAEntry rna(sequence, structure);
-    knotergy::RNAProcessedEntry processed_rna(rna);
-    knotergy::LoopFactory factory(processed_rna);
-    knotergy::ComputeEnergy energy(factory.get_root_node(), sequence);
+    float energy = pipeline(sequence, structure);
 
-    EXPECT_NEAR(energy.getEnergy(), -28.10, 0.001);
+    EXPECT_NEAR(energy, -28.10, 0.001);
 }
 
 
@@ -64,53 +52,39 @@ TEST(NonPseudoKnottedEnergies, MultipleStacks) {
 TEST(NonPseudoKnottedEnergies, MultiLoopWithStacks) {
     std::string sequence = "GGGGGGAAAAAGGGGGGAAAAGGGGGGCCCCCCAAAAAACCCCCCAAAAGGGGGGAAAAAACCCCCCAAAGGGAAACCCCCCCCCA";
     std::string structure = "...........((((((....((((((............))))))....((((((......))))))...((....)).)))))).";
-    knotergy::load_energy_parameters();
-    knotergy::RNAEntry rna(sequence, structure);
-    knotergy::RNAProcessedEntry processed_rna(rna);
-    knotergy::LoopFactory factory(processed_rna);
-    knotergy::ComputeEnergy energy(factory.get_root_node(), sequence);
+    float energy = pipeline(sequence, structure);
 
-    EXPECT_NEAR(energy.getEnergy(), -41.10, 0.001);
+    EXPECT_NEAR(energy, -41.10, 0.001);
 }
 
 // echo -e "AAAAAAGGGGCCCCCCAAAAAAAAAAAAGGGGGGAAAAGGGGGGUUUUUUCCCCCCAAAUUUUUUAAGUUUUUU\n((((((....((((((............))))))....((((((......))))))...((....)).))))))" | RNAeval
 TEST(NonPseudoKnottedEnergies, MultiLoopWithStacksFull) {
     std::string sequence = "AAAAAAGGGGCCCCCCAAAAAAAAAAAAGGGGGGAAAAGGGGGGUUUUUUCCCCCCAAAUUUUUUAAGUUUUUU";
     std::string structure = "((((((....((((((............))))))....((((((......))))))...((....)).))))))";
-    knotergy::load_energy_parameters();
-    knotergy::RNAEntry rna(sequence, structure);
-    knotergy::RNAProcessedEntry processed_rna(rna);
-    knotergy::LoopFactory factory(processed_rna);
-    knotergy::ComputeEnergy energy(factory.get_root_node(), sequence);
+    float energy = pipeline(sequence, structure);
 
-    EXPECT_NEAR(energy.getEnergy(), -21.9, 0.001);
+    EXPECT_NEAR(energy, -21.9, 0.001);
 }
 
 // echo -e "GGGGUUAUUUUAUUAAAAAUAACCCUGGUUUUUAAGGCGGGGUCGUGCGGUAAGGGAACCC\n((((..(...).((...)))..(((.(.((...))..(...).)..(...)..)))..)))" | RNAeval
 TEST(NonPseudoKnottedEnergies, MultiWithMultiLoop) {
     std::string sequence = "GGGGUUAUUUUAUUAAAAAUAACCCUGGUUUUUAAGGCGGGGUCGUGCGGUAAGGGAACCC";
     std::string structure = "((((..(...).((...)))..(((.(.((...))..(...).)..(...)..)))..)))";
-    knotergy::load_energy_parameters();
-    knotergy::RNAEntry rna(sequence, structure);
-    knotergy::RNAProcessedEntry processed_rna(rna);
-    knotergy::LoopFactory factory(processed_rna);
-    knotergy::ComputeEnergy energy(factory.get_root_node(), sequence);
+    float energy = pipeline(sequence, structure);
 
-    EXPECT_NEAR(energy.getEnergy(), 30.90, 0.001);
+    EXPECT_NEAR(energy, 30.90, 0.001);
 }
-
-
 
 TEST(NonPseudoKnottedEnergies, PositiveInternalLoops) {
     std::string sequence  = "GGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAACCCUUUGGGAAAUUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGGAUUCGAGCGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACGUACG";
     std::string structure = "(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...(((...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))...)))..........((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..((((((..........))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))..))))))(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((....)))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))";
-    float result = pipeline(sequence, structure);
-    EXPECT_NEAR(result, -471.90, 0.001);
+    float energy = pipeline(sequence, structure);
+    EXPECT_NEAR(energy, -471.90, 0.001);
 } 
 
 TEST(NonPseudoKnottedEnergies, MoreLongSequences) {
     std::string sequence  = "UUAAAAGGGAUGCCUCUCCUGUUCAUCUUGUGGAGAAGCAUUCGAUAAGGUCAUCAUAAUGGGUCCAGCUUUGCGACCUGGCGAGAUUAGUCAGGAAAAUGUGAAGUGGGUCUUCGCUUUCCAGGUACAGGAGGCUCGCCCCGCUCAUCCAGUUCGUCCCCUAACCACUUGUUUUCUCAGGAUAGUUUGUUUUGUACACCCGUGUACAUACACAUGUAUCACACCCCAGAUUGCCGAAUGUUUCGUUCGGUCGAGCCUGACUAUGCAUAAACCCUACCUCUGAAACCUUGGGCAACUCACUACUUCCGAGCUAAAUCCCUCUGUUUGAGCUAGCCUGAGAUUUCAACUGGCUUCGGCCUUGUUUAUACCAUCGUUUGCUGAUCCAUUGAAGAAAUAAGUUACCGAUGGCCCCAAACUGACGAUCACUAUUCUUUCCAUAGGAGUUAUGGGUAUACUGCCCGUAGACGGAAAGAUGAAUGCCUGUAUCCGGGAGUCAGAUG";
     std::string structure = ".......((((((.(((((............))))).)))))).....(((((((..((((((((.(((...((((((((((.......))))))...(((.(..((((((((((..............))))))))))....).)))....................(((...(((((((.((((((.............((((((((....))))).)))....((((..((((((((...))))))))...((((((.....((.............)).....))))))..(((.........))).........))))...)))))).)))))))...)))..(((....)))............))))..)))))))))))..(....)........))))))).....(((((...((....(((((((...(...(((((((.......))))))).))))))))....))((((....)))).)))))...";
-    float result = pipeline(sequence, structure);
-    EXPECT_NEAR(result, -121.2, 0.001);
+    float energy = pipeline(sequence, structure);
+    EXPECT_NEAR(energy, -121.2, 0.001);
 }   
