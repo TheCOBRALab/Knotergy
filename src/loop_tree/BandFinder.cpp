cbcff1600b11d257124253fe300c80089c34ab66
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-08-11 12:07:25 -0600

    change aux_band -> band_links

--- src/loop_tree/BandFinder.cpp
+++ src/loop_tree/BandFinder.cpp
@@ -20,9 +20,9 @@ std::vector<Band> BandFinder::find_bands(const size_t& left_bound, const size_t&
         THROW_ERROR("Right bound exceeds the size of structure.");
     }
 
-    std::unordered_map<size_t, Aux_Band> aux_bands = generate_Aux_Band_list(left_bound, right_bound, pairings, cr_pairings);
+    std::unordered_map<size_t, BandLink> band_links = generate_band_links(left_bound, right_bound, pairings, cr_pairings);
     
-    if (aux_bands.empty()) {
+    if (band_links.empty()) {
         return bands;  // No bands found
     }
 
@@ -43,7 +43,7 @@ std::vector<Band> BandFinder::find_bands(const size_t& left_bound, const size_t&
         size_t j_prime = j;
 
         // walks the stem until last base pair in the given region (finds i` and j`)
-        while (extend_stem(i_prime, j_prime, aux_bands, pairings)) {
+        while (extend_stem(i_prime, j_prime, band_links, pairings)) {
         }
 
         // stores entire band
@@ -58,11 +58,11 @@ std::vector<Band> BandFinder::find_bands(const LoopNode& node, const RNAProcesse
     return find_bands(node.begin, node.end, node.loop_type, processed_rna.get_pairings(), processed_rna.get_closed_regions_pairings());
 }
 
-bool BandFinder::extend_stem(size_t& i_prime, size_t& j_prime, const std::unordered_map<size_t, Aux_Band>& aux_bands, const std::vector<size_t>& pairings) {
+bool BandFinder::extend_stem(size_t& i_prime, size_t& j_prime, const std::unordered_map<size_t, BandLink>& band_links, const std::vector<size_t>& pairings) {
     
-    if (aux_bands.find(i_prime) != aux_bands.end() && aux_bands.find(j_prime) != aux_bands.end()) {
-        size_t i_tmp = aux_bands.at(i_prime).next;
-        size_t j_tmp = aux_bands.at(j_prime).prev;
+    if (band_links.find(i_prime) != band_links.end() && band_links.find(j_prime) != band_links.end()) {
+        size_t i_tmp = band_links.at(i_prime).next;
+        size_t j_tmp = band_links.at(j_prime).prev;
         std::cout << "i_tmp: " << i_tmp << std::endl;
         std::cout << "j_tmp: " << j_tmp << std::endl;
 
@@ -82,56 +82,57 @@ bool BandFinder::extend_stem(size_t& i_prime, size_t& j_prime, const std::unorde
     }
 }
 
-std::unordered_map<size_t, Aux_Band> const BandFinder::generate_Aux_Band_list(const size_t& left_bound,
+std::unordered_map<size_t, BandLink> const BandFinder::generate_band_links(const size_t& left_bound,
                                                                           const size_t& right_bound,
                                                                           const std::vector<size_t>& pairings,
                                                                           const std::vector<size_t>& cr_pairings){
     if (right_bound < left_bound) return {};
     
-
-    
-    size_t start_idx = left_bound;
-
-    // finds the first paired base in the region (Normally it's paired, but just in case)
-    [[unlikely]] while (pairings[start_idx] == NULL_INDEX){
-        ++start_idx;
+    if (pairings[left_bound] == NULL_INDEX){
+        THROW_ERROR("Left index is not a base-pair");
+    }
+    if (pairings[right_bound] == NULL_INDEX){
+        THROW_ERROR("Right index is not a base-pair");
+    }
+    if (cr_pairings[left_bound] != right_bound){
+        THROW_ERROR("Left bound does not pair with right bound");
     }
     
-    std::unordered_map<size_t, Aux_Band> aux_bands;
-    aux_bands.emplace(start_idx, Aux_Band(start_idx));
-    size_t current_key = start_idx;
+    
+    std::unordered_map<size_t, BandLink> band_links;
+    band_links.emplace(left_bound, BandLink{left_bound});
+    size_t current_key = left_bound;
 
-    for (size_t i = start_idx + 1; i < right_bound; ++i){
+    for (size_t i = left_bound + 1; i < right_bound; ++i){
 
+        // skip unpaired
         if (pairings[i] == NULL_INDEX){
             continue;
         }
 
+        // skip closed regions
         if (cr_pairings[i] != NULL_INDEX && i != left_bound) {
             i = cr_pairings[i];
             continue;
         }
 
-        if (aux_bands.find(current_key) == aux_bands.end()) {
-            THROW_ERROR("Current key not found in aux_bands");
+        if (band_links.find(current_key) == band_links.end()) {
+            THROW_ERROR("Current key not found in band_links");
         }
         
         std::cout << "Adding aux band for i: " << i << std::endl;
-        aux_bands[current_key].next = i;
-        aux_bands[i] = Aux_Band(i, current_key);
+        band_links[current_key].next = i;
+        band_links[i] = BandLink{i, current_key};
         current_key = i;
     }
+    // process last link
+    band_links[current_key].next = right_bound;
+    band_links[right_bound] = BandLink{right_bound, current_key};
 
-    // Normally the right_bound is paired, but just in case
-    [[likely]] if (pairings[right_bound] != NULL_INDEX) {
-        aux_bands[current_key].next = right_bound;
-        aux_bands[right_bound] = Aux_Band(right_bound, current_key);
-    }
-
-    return aux_bands;
+    return band_links;
 }
-std::unordered_map<size_t, Aux_Band> const BandFinder::generate_Aux_Band_list(const LoopNode& node, const RNAProcessedEntry& processed_entry){
-    return generate_Aux_Band_list(node.begin, node.end, processed_entry.get_pairings(), processed_entry.get_closed_regions_pairings());
+std::unordered_map<size_t, BandLink> const BandFinder::generate_band_links(const LoopNode& node, const RNAProcessedEntry& processed_entry){
+    return generate_band_links(node.begin, node.end, processed_entry.get_pairings(), processed_entry.get_closed_regions_pairings());
 }
 
 }  // namespace knotergy
--- src/loop_tree/BandFinder.hpp
+++ src/loop_tree/BandFinder.hpp
@@ -6,15 +6,10 @@
 #include "../preprocessing/RNAProcessedEntry.hpp"
 namespace knotergy {
 
-struct Aux_Band {
+struct BandLink {
     size_t value = NULL_INDEX;
     size_t prev = NULL_INDEX;
     size_t next = NULL_INDEX;
-    
-    Aux_Band() = default;  
-    Aux_Band (size_t v) : value{v} {}
-    Aux_Band (size_t v, size_t p) : value{v}, prev{p} {}
-    Aux_Band (size_t v, size_t p, size_t n) : value{v}, prev{p}, next{n} {}
 };
 
 class BandFinder {
@@ -31,14 +26,14 @@ class BandFinder {
    private:
     static bool extend_stem(size_t& i_prime,
                             size_t& j_prime,
-                            const std::unordered_map<size_t, Aux_Band>& aux_bands,
+                            const std::unordered_map<size_t, BandLink>& aux_bands,
                             const std::vector<size_t>& pairings);
 
-    static std::unordered_map<size_t, Aux_Band> const generate_Aux_Band_list(const size_t& left_bound,
+    static std::unordered_map<size_t, BandLink> const generate_band_links(const size_t& left_bound,
                                                                    const size_t& right_bound,
                                                                    const std::vector<size_t>& pairings,
                                                                    const std::vector<size_t>& cr_pairings);
 
-    static std::unordered_map<size_t, Aux_Band> const generate_Aux_Band_list(const LoopNode& node, const RNAProcessedEntry& processed_entry);
+    static std::unordered_map<size_t, BandLink> const generate_band_links(const LoopNode& node, const RNAProcessedEntry& processed_entry);
     };
 }  // namespace knotergy
