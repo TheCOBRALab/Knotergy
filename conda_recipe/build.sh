bf194419fab5757fc2270bcc7082a7e8da5e8592
Yahya Al-Shamali <yaalshama@gmail.com>
2025-10-20 16:05:47 -0600

    Fix Conda build.sh

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -29,13 +29,25 @@ cd   ViennaRNA-2.7.0
 
 # Bug fixes in dlib until ViennaRNA updates to a newer version
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    # typedef with the standard UTF-32 code unit.
+    sed -i.bak 's/typedef uint32 unichar;/typedef char32_t unichar;/' \
+        src/dlib-19.24/dlib/unicode/unicode.h
+
+    # make the new type serialisable
+    sed -i.bak '/USE_DEFAULT_INT_SERIALIZATION_FOR(uint64)/a\
+    USE_DEFAULT_INT_SERIALIZATION_FOR(char32_t)
+    ' src/dlib-19.24/dlib/serialize.h
+
     # “template” keyword.  Add the empty <> that upstream already has.
     sed -i.bak 's/::template go(/::template go<>(/' \
         src/dlib-19.24/dlib/global_optimization/find_max_global.h
 fi
 
-./configure --without-perl --prefix="${PREFIX}"
+./configure --without-perl --without-python --prefix="${PREFIX}"
 make  -j"${CPU_COUNT}"
+
+# override pkgpyexecdir so "make install" never touches /RNA (/RNA needs sudo access)
+make  -j"${CPU_COUNT}" pkgpyexecdir="${PY_SITE_DIR}" install
 
 cd .. # back to build dir
 
